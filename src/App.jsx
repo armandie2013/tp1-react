9682df0ca75b72afa1c3d627995e1e06a7a8ec92
= <=>
2025-06-06 14:17:03 -0300

    Maquetado

--- src/App.jsx
+++ src/App.jsx
@@ -5,9 +5,13 @@ import NavBar from "./components/NavBar";
 function App() {
   return (
     <>
-      <NavBar />
-      <Main/>
-      <Footer/>
+      <div className="flex flex-col min-h-screen">
+        <NavBar />
+        <main className="flex-grow">
+          <Main />
+        </main>
+        <Footer />
+      </div>
     </>
   );
 }
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,23 +1,28 @@
 import React, { useState } from 'react';
+import { motion } from 'framer-motion';
 
 const Main = () => {
   const [mensajeMostrado, setMensajeMostrado] = useState(false);
-
   const manejarClick = () => {
-    setMensajeMostrado(true);
+    setMensajeMostrado(!mensajeMostrado);
   };
 
   return (
     <div
-      className="w-full h-screen bg-cover bg-center flex flex-col items-center justify-center text-white px-4"
+      className="w-full min-h-[calc(100vh-130px)] bg-cover bg-center flex flex-col items-center justify-center text-white px-4"
       style={{ backgroundImage: "url('/fondo-superheroes-4.jpg')" }}
     >
-      <button
+      <motion.button
         onClick={manejarClick}
+        initial={{scale:1000}}
+        animate={{opacity: 1, scale:1}}
+        whileHover={{scale:2}}
+        whileTap={{scale:0.95}}
+        transition={{duration:0.4}}
         className="bg-red-600 hover:bg-red-700 text-white font-bold py-3 px-6 rounded-lg shadow-lg text-lg mb-4"
       >
         Presioname
-      </button>
+      </motion.button>
 
       {mensajeMostrado && (
         <p className="bg-black/60 p-4 rounded-lg text-lg md:text-xl">
